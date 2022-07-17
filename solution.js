c5bfa99525cdc857bbdd67b282f11db28efba31d
xstartstaticx <daniel.reiller@icloud.com>
2022-07-17 18:23:31 +0200

    finished exercise

--- solution.js
+++ solution.js
@@ -1,29 +1,38 @@
-//Q1 Store Mark's and John's mass and height in variables.
-let markHeight = 1.65;
-let markMass = 72.5;
-let johnHeight = 1.87;
-let johnMass = 68;
-
-//Q2 Calculate both their BMIs and store their BMIs in variables.
-let markBMI = markMass / (markHeight * markHeight);
-console.log(markBMI);
-let johnBMI = johnMass / (johnHeight * johnHeight);
-console.log(johnBMI);
-
-//Q3 Create a boolean variable containing information about whether Mark has a higher BMI than John.
-const markMoreBeefCake = markBMI >= johnBMI;
-console.log("Q3:", markMoreBeefCake);
-
-//Q4 Print a string to the console containing the variable from step 3 using string interpolation. (e.g. "Is Mark's BMI higher than John's? Why yes, it's [boolean], it is").
-console.log(
-  "Q4: Is Mark's BMI higher than John's? Why yes, it's",
-  markMoreBeefCake,
-  "it is"
-);
-
-//Q5 Create an *if* statement which prints the name and BMI of the person with the highest BMI.
-if (markBMI > johnBMI) {
-  console.log("Q5: Marks BMI is higher, it is", markBMI.toFixed(2));
-} else {
-  console.log("Q5: Johns BMI is higher, it is", johnBMI.toFixed(2));
+//  i
+function calculateBMI(name, weight, height) {
+  BMI = weight / (height * height);
+  console.log(name, BMI);
 }
+calculateBMI("Mark", 72.5, 1.65);
+calculateBMI("John", 68, 1.87);
+
+// ii
+
+function whoIsBiggest(name1, name2, bmi1, bmi2) {
+  if (bmi1 > bmi2) {
+    console.log(name1, "is bigger");
+  } else {
+    console.log(name2, "is bigger");
+  }
+}
+whoIsBiggest("Mark", "John", 26.629935720844816, 19.445794846864363);
+
+// Bonus
+function whoIsBiggest(
+  name1,
+  name2,
+  name1weight,
+  name2weight,
+  name1height,
+  name2height
+) {
+  if (
+    name1weight / (name1height * name1height) >
+    name2weight / (name2height * name2height)
+  ) {
+    console.log(name1);
+  } else {
+    console.log(name2);
+  }
+}
+whoIsBiggest("Mark", "John", 72.5, 68, 1.65, 1.87);
